e02fc48a8115940c6f25ac9cc5fa18ea73bf1497
sydsimon <sydsimon@sas.upenn.edu.com>
2024-03-19 13:58:29 -0400

    updated scraper

--- script.py
+++ script.py
@@ -26,7 +26,7 @@ def scrape_data_point():
 
     if req.ok:
         soup = bs4.BeautifulSoup(req.text, "html.parser")
-        target_element = soup.find("a", class_="frontpage-link")
+        target_element = soup.find("a", class_="medium-link")
         data_point = "" if target_element is None else target_element.text
         loguru.logger.info(f"Data point: {data_point}")
         return data_point
@@ -48,7 +48,7 @@ if __name__ == "__main__":
     # Load daily event monitor
     loguru.logger.info("Loading daily event monitor")
     dem = daily_event_monitor.DailyEventMonitor(
-        "data/daily_pennsylvanian_headlines.json"
+        "data/daily_pennsylvanian_multimedia_headlines.json.json"
     )
 
     # Run scrape
